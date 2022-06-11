2ded71f1a18e2141fff70546b9e7830375dd13d8
majnurangeela <84721324+majnurangeela@users.noreply.github.com>
2022-06-11 14:11:32 +0530

    Added Multi SHarer Support

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -19,7 +19,8 @@ from cfscrape import create_scraper
 from bs4 import BeautifulSoup
 from base64 import standard_b64encode
 from lxml import etree
-from bot import LOGGER, UPTOBOX_TOKEN, CRYPT, APPDRIVE_EMAIL, APPDRIVE_PASS
+import cloudscraper
+from bot import LOGGER, UPTOBOX_TOKEN, CRYPT, UNIFIED_EMAIL, UNIFIED_PASS, HUBDRIVE_CRYPT, KATDRIVE_CRYPT, DRIVEFIRE_CRYPT, XSRF_TOKEN, laravel_session
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.ext_utils.bot_utils import *
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
@@ -422,8 +423,8 @@ def gdtot(url: str) -> str:
     return f'https://drive.google.com/open?id={decoded_id}'
   
 account = {
-   'email': APPDRIVE_EMAIL, 
-   'passwd': APPDRIVE_PASS
+   'email': UNIFIED_EMAIL, 
+   'passwd': UNIFIED_PASS
 }
 
 def account_login(client, url, email, password):
@@ -450,9 +451,9 @@ def parse_infou(data):
     return info_parsed
 
 def appdrive(url: str) -> str:
-    if (APPDRIVE_EMAIL or APPDRIVE_PASS) is None:
-        raise DirectDownloadLinkException("APPDRIVE_EMAIL and APPDRIVE_PASS env vars not provided")
-    client = requests.Session()
+    if (UNIFIED_EMAIL or UNIFIED_PASS) is None:
+        raise DirectDownloadLinkException("UNIFIED_EMAIL and UNIFIED_PASS env vars not provided")
+    client = cloudscraper.create_scraper(delay=10, browser='chrome')
     client.headers.update({
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
     })
@@ -543,4 +544,129 @@ def appdrive(url: str) -> str:
     flink = info_parsed['gdrive_link']
     info_parsed['src_url'] = url
     
+    return flink
+  
+def parse_info(res, url):
+    info_parsed = {}
+    #title = re_findall('>(.*?)<\/h4>', res.text)[0] --- Not Important
+    if 'drivebuzz' in url:
+        info_chunks = findall('<td\salign="right">(.*?)<\/td>', res.text)
+    else:
+        info_chunks = findall('>(.*?)<\/td>', res.text)
+    #info_parsed['title'] = title  --- Not Important
+    for i in range(0, len(info_chunks), 2):
+        info_parsed[info_chunks[i]] = info_chunks[i+1]
+    return info_parsed
+  
+def udrive(url: str) -> str:
+    client = cloudscraper.create_scraper(delay=10, browser='chrome')
+    if 'hubdrive' in url:
+        client.cookies.update({'crypt': HUBDRIVE_CRYPT})
+    if 'drivehub' in url:
+        client.cookies.update({'crypt': KATDRIVE_CRYPT})
+    if 'katdrive' in url:
+        client.cookies.update({'crypt': KATDRIVE_CRYPT})
+    if 'kolop' in url:
+        client.cookies.update({'crypt': KATDRIVE_CRYPT})
+    if 'drivefire' in url:
+        client.cookies.update({'crypt': DRIVEFIRE_CRYPT})
+    if 'drivebuzz' in url:
+        client.cookies.update({'crypt': DRIVEFIRE_CRYPT})
+        
+    res = client.get(url)
+    info_parsed = parse_info(res, url)
+    
+    #if 'drivebuzz' not in url:
+    info_parsed['error'] = False
+    
+    up = urlparse(url)
+    req_url = f"{up.scheme}://{up.netloc}/ajax.php?ajax=download"
+    
+    file_id = url.split('/')[-1]
+    
+    data = { 'id': file_id }
+    
+    headers = {
+        'x-requested-with': 'XMLHttpRequest'
+    }
+    
+    try:
+        res = client.post(req_url, headers=headers, data=data).json()['file']
+    except: return {'error': True, 'src_url': url}
+    
+    if 'drivefire' in url:
+        decoded_id = res.rsplit('/', 1)[-1]
+        flink = f"https://drive.google.com/file/d/{decoded_id}"
+        return flink
+    elif 'drivehub' in url:
+        gd_id = res.rsplit("=", 1)[-1]
+        flink = f"https://drive.google.com/open?id={gd_id}"
+        return flink
+    elif 'drivebuzz' in url:
+        gd_id = res.rsplit("=", 1)[-1]
+        flink = f"https://drive.google.com/open?id={gd_id}"
+        return flink
+    else:
+        gd_id = findall('gd=(.*)', res, re.DOTALL)[0]
+ 
+    info_parsed['gdrive_url'] = f"https://drive.google.com/open?id={gd_id}"
+    info_parsed['src_url'] = url
+    flink = info_parsed['gdrive_url']
+
+    return flink  
+
+def parse_infos(res):
+    f = findall(">(.*?)<\/td>", res.text)
+    info_parsed = {}
+    for i in range(0, len(f), 3):
+        info_parsed[f[i].lower().replace(' ', '_')] = f[i+2]
+    return info_parsed
+
+def sharer_pw(url, forced_login=False):
+    client = cloudscraper.create_scraper(delay=10, browser='chrome')
+    
+    client.cookies.update({
+        "XSRF-TOKEN": XSRF_TOKEN,
+        "laravel_session": laravel_session
+    })
+    
+    res = client.get(url)
+    token = findall("token\s=\s'(.*?)'", res.text, re.DOTALL)[0]
+    
+    ddl_btn = etree.HTML(res.content).xpath("//button[@id='btndirect']")
+    
+    info_parsed = parse_infos(res)
+    info_parsed['error'] = True
+    info_parsed['src_url'] = url
+    info_parsed['link_type'] = 'login' # direct/login
+    info_parsed['forced_login'] = forced_login
+    
+    headers = {
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'x-requested-with': 'XMLHttpRequest'
+    }
+    
+    data = {
+        '_token': token
+    }
+    
+    if len(ddl_btn):
+        info_parsed['link_type'] = 'direct'
+    if not forced_login:
+        data['nl'] = 1
+    
+    try: 
+        res = client.post(url+'/dl', headers=headers, data=data).json()
+    except:
+        return info_parsed
+    
+    if 'url' in res and res['url']:
+        info_parsed['error'] = False
+        info_parsed['gdrive_link'] = res['url']
+        
+    if len(ddl_btn) and not forced_login and not 'url' in info_parsed:
+        # retry download via login
+        return sharer_pw(url, forced_login=True)
+    
+    flink = info_parsed['gdrive_link']
     return flink

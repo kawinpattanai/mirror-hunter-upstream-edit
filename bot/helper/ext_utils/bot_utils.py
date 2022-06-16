20707906afe1667c8b540d3094699a6a5a9fdc7a
majnurangeela <84721324+majnurangeela@users.noreply.github.com>
2022-06-17 01:14:21 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -123,7 +123,7 @@ def get_readable_message():
                 globals()['PAGE_NO'] -= 1
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
             msg += f"<b>Name:</b> <code>{escape(str(download.name()))}</code>"
-            msg += f"\n<b>/nStatus:</b> <i>{download.status()}</i>"
+            msg += f"\n\n<b>Status:</b> <i>{download.status()}</i>"
             if download.status() not in [
                 MirrorStatus.STATUS_ARCHIVING,
                 MirrorStatus.STATUS_EXTRACTING,
@@ -136,7 +136,7 @@ def get_readable_message():
                 elif download.status() == MirrorStatus.STATUS_UPLOADING:
                     msg += f"\n<b>⬆️ Uploaded:</b> {get_readable_file_size(download.processed_bytes())}\n<b>📦 Total Size:</b>{download.size()}"
                 else:
-                    msg += f"\n<b>⬇️ Downloaded:</b> {get_readable_file_size(download.processed_bytes())}\t<b>📦 Total Size:</b>{download.size()}"
+                    msg += f"\n<b>⬇️ Downloaded:</b> {get_readable_file_size(download.processed_bytes())}\n<b>📦 Total Size:</b>{download.size()}"
                 msg += f"\n<b>🏎️ Speed:</b> {download.speed()}\n<b>⏳ ETA:</b> {download.eta()}"
                 try:
                     msg += f"\n\n<b>🌱 Seeders:</b> {download.aria_download().num_seeders}" \
